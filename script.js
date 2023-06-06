54fbfca3a955e0bb38e4ae8786cd7dfc0fb2970a
Renan <112997019+Renan-Rezende@users.noreply.github.com>
2023-06-06 12:35:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function gerar() {
+
+  //
+
+  var nome1 = document.getElementById("nome1").value
+  var nome_texto = document.getElementById("nome-text1")
+  nome_texto.textContent = nome1
+
+  var nome2 = document.getElementById("nome2").value
+  var nome_texto = document.getElementById("nome-text2")
+  nome_texto.textContent = nome2
+
+  var nome3 = document.getElementById("nome3").value
+  var nome_texto = document.getElementById("nome-text3")
+  nome_texto.textContent = nome3
+
+
+
   for (let i = 1; i < 4; i++) {
+
+   
     var RandomNumber;
     var numbers = [];
     do {
@@ -9,6 +28,7 @@ function gerar() {
       }
     } while (numbers.length !== 25);
     console.log(numbers);
+
 
     const texto = document.getElementsByClassName("CT" + i);
     for (let i = 0; i < texto.length; i++) {
